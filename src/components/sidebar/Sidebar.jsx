d84b5365a509abd8eae7a6d9be9eda4e835a21fb
Commude-websys <ymkmy98@gmail.com>
2023-05-15 22:56:34 +0900

    Introducing MUI

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			{/* twitter icon */}
+			<TwitterIcon className="sidebar_twitterIcon" />
+		</div>
+	);
+
+	// SidebarOption
+
+	// tweet button
+};
+
+export default Sidebar;
